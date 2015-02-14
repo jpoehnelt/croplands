06ad7527487e0089bfd0e7dc8ef4f7ff1bd5028d
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-14 08:43:38 -0700

    add test imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,13 @@
-# import unittest
-#
-# from test_auth_views import TestAuthViews
-# from test_email import TestEmail
-# from test_api import TestApi
-# from test_utils_s3 import TestUtilsS3
-# from test_db_models import TestDatabase
-# from test_countries import TestCountries
-# from test_tasks import TestTasks
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+
+
+from test_auth_views import TestAuthViews
+from test_email import TestEmail
+from test_api import TestApi
+from test_utils_s3 import TestUtilsS3
+from test_db_models import TestDatabase
+from test_countries import TestCountries
+from test_tasks import TestTasks
+
+if __name__ == '__main__':
+    unittest.main()
