71166c2a3fe07fac9a4afbeef5189169f88fbbf2
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-14 10:09:15 -0700

    removed config on rabbit external

--- gfsad/config/testing.py
+++ gfsad/config/testing.py
@@ -1,4 +1,7 @@
-from default import *
+# from default import *
+import os
+from datetime import timedelta
+
 
 TESTING = True
 DEBUG = True
@@ -8,6 +11,16 @@ POSTMARK_API_KEY = 'POSTMARK_API_TEST'
 REDISCLOUD_URL = 'redis://127.0.0.1:6379'
 RATELIMIT_STORAGE_URL = 'redis://127.0.0.1:6379'
 # SQLALCHEMY_ECHO = True
-
 SECRET_KEY = os.urandom(24)
 CELERY_TIMEZONE = 'UTC'
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+
+AWS_URL_SIGNING_EXPIRATION_DEFAULT = 60 * 30
+
+JWT_AUTH_URL_RULE = '/auth/l'
+JWT_EXPIRATION_DELTA = timedelta(hours=6)
+
+DEFAULT_MAIL_SENDER = 'info@croplands.org'
+
+AUTH_RESET_TOKEN_EXPIRATION = 300
+AUTH_REQUIRE_CONFIRMATION = False
