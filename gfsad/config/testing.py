da8fb382cda9bc3be847cece51a7ece8236a7773
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-15 09:14:17 -0700

    changed up config to load defaults in testing

--- gfsad/config/testing.py
+++ gfsad/config/testing.py
@@ -1,4 +1,4 @@
-# from default import *
+from default import *
 import os
 from datetime import timedelta
 
@@ -6,7 +6,6 @@ from datetime import timedelta
 TESTING = True
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = "sqlite://"
-CELERY_DEFAULT_QUEUE = 'gfsad-testing'
 POSTMARK_API_KEY = 'POSTMARK_API_TEST'
 REDISCLOUD_URL = 'redis://127.0.0.1:6379'
 RATELIMIT_STORAGE_URL = 'redis://127.0.0.1:6379'
@@ -15,12 +14,3 @@ SECRET_KEY = os.urandom(24)
 CELERY_TIMEZONE = 'UTC'
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_ALWAYS_EAGER = False
-AWS_URL_SIGNING_EXPIRATION_DEFAULT = 60 * 30
-
-JWT_AUTH_URL_RULE = '/auth/l'
-JWT_EXPIRATION_DELTA = timedelta(hours=6)
-
-DEFAULT_MAIL_SENDER = 'info@croplands.org'
-
-AUTH_RESET_TOKEN_EXPIRATION = 300
-AUTH_REQUIRE_CONFIRMATION = False
