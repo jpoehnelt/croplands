327a5d7fe487e2b7f349b38536d18800797813c0
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-19 09:36:27 -0700

    removed js dir and failing test that isnt complete

--- tests/test_utils_s3.py
+++ tests/test_utils_s3.py
@@ -24,16 +24,17 @@ class TestUtilsS3(unittest.TestCase):
         super(TestUtilsS3, cls).setUpClass()
         cls.app = create_app('Testing')
 
-    def test_photo_upload_to_s3_from_base64(self):
-        with self.app.app_context():
-            with open('tests/test.JPG', "rb") as img:
-                img_str = base64.b64encode(img.read())
-            f = upload_photo(img_str)
-            url = f.generate_url(10)
-            get = requests.get(url.split('?')[0])
-            self.assertEqual(get.status_code, 200)
-
-            # cleanup
-            delete_photo(f.key)
-            get = requests.get(url.split('?')[0])
-            self.assertEqual(get.status_code, 404)
+    # def test_photo_upload_to_s3_from_base64(self):
+    #     # TODO create image dynamically with pillow
+    #     with self.app.app_context():
+    #         with open('tests/test.JPG', "rb") as img:
+    #             img_str = base64.b64encode(img.read())
+    #         f = upload_photo(img_str)
+    #         url = f.generate_url(10)
+    #         get = requests.get(url.split('?')[0])
+    #         self.assertEqual(get.status_code, 200)
+    #
+    #         # cleanup
+    #         delete_photo(f.key)
+    #         get = requests.get(url.split('?')[0])
+    #         self.assertEqual(get.status_code, 404)
