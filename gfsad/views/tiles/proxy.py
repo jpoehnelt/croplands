de8b3d6628c035d53898f8b9709c3feb9795c638
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-02 00:21:19 -0500

    testing proxy

--- gfsad/views/tiles/proxy.py
+++ gfsad/views/tiles/proxy.py
@@ -3,7 +3,9 @@ import requests
 from gfsad import limiter
 from gfsad.views.tiles import tile_blueprint
 from gfsad.views.tiles.gee import get_map, build_url
+import time
 
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 @tile_blueprint.route("/<asset>/<x>/<y>/<z>")
 @limiter.limit("1000 per minute")
@@ -17,14 +19,21 @@ def tile_proxy(x, y, z, asset):
     :param asset:
     :return:
     """
+    time_start = current_milli_time()
     map_args = request.args.__dict__
     map_args['asset'] = asset
 
     map = get_map(**map_args)
 
+    time_mid = current_milli_time()
+
     url = build_url(map['mapid'], map['token'], int(x), int(y), int(z))
     req = requests.get(url, stream=True)
+    print "time to map_id: %dms, to gee response: %dms" % (time_mid - time_start, current_milli_time() - time_mid)
 
-    stream = stream_with_context(req.iter_content())
-    return Response(stream, content_type=req.headers['content-type'])
+    def generate():
+        for chunk in req.iter_content(1024):
+            yield chunk
+
+    return Response(generate(), content_type=req.headers['content-type'])
 
