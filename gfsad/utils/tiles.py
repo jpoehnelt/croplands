34a425871fad201b01cdc0c231f2c0e5ee161a2a
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-17 20:41:44 -0500

    converts a tile number to lat,lon

--- gfsad/utils/tiles.py
+++ gfsad/utils/tiles.py
@@ -14,3 +14,18 @@ def degree_to_tile_number(lat_deg, lon_deg, zoom):
     x = int((lon_deg + 180.0) / 360.0 * n)
     y = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return x, y
+
+
+def tile_number_to_degree(x, y, zoom):
+    """
+
+    :param x:
+    :param y:
+    :param zoom:
+    :return:
+    """
+    n = 2.0 ** zoom
+    lon_deg = x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
+    lat_deg = math.degrees(lat_rad)
+    return lat_deg, lon_deg
