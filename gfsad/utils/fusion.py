1f09fef0da3e90f2eace6b1f87f35d544c6f9fdc
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-18 15:42:58 -0500

    added functionality for updating google fusion table to get data into google earth engine

---
+++ gfsad/utils/fusion.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from flask import current_app
+from oauth2client.client import SignedJwtAssertionCredentials
+import httplib2
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+
+FUSION_TABLE_SCOPE = 'https://www.googleapis.com/auth/fusiontables'
+FUSION_TABLE_TEST = '1y6rdRvEPXW4r2zXHterHoDrGUVYxnUH_saPMdaCo'
+
+
+def init_google_service(scope=FUSION_TABLE_SCOPE, name='fusiontables', version='v2'):
+    """
+    Creates a new API service for interacting with the resource.
+    :param scope: string of the url
+    :param name: string of the name of the resource
+    :param version: string of the version
+    :return: service
+    """
+    # Create a new API service for interacting with Fusion Tables
+    credentials = SignedJwtAssertionCredentials(current_app.config['GOOGLE_SERVICE_ACCOUNT'],
+                                                current_app.config['GOOGLE_API_KEY'], scope)
+    return build(name, version, http=credentials.authorize(httplib2.Http()))
+
+
+def replace_rows(table_id, fd):
+    """
+    Replaces all rows in a fusion table with the fd of a csv.
+    :param table_id: string
+    :param fd: file descriptor
+    :return: None
+    """
+    service = init_google_service()
+
+    media_body = MediaIoBaseUpload(fd, mimetype='application/octet-stream')
+
+    return service.table().replaceRows(tableId=table_id, media_body=media_body).execute()
+
+
---
+++ tests/test_fusion_tables.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from gfsad import create_app, limiter
+from gfsad.models import db
+from gfsad.utils.fusion import FUSION_TABLE_SCOPE, init_google_service, replace_rows
+import csv
+from cStringIO import StringIO
+
+
+class TestFusionTables(TestCase):
+    app = None
+
+    def setUp(self):
+        self.app = TestFusionTables.app
+        with self.app.app_context():
+            limiter.enabled = False
+            db.create_all()
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestFusionTables, cls).setUpClass()
+        cls.app = create_app('Testing')
+
+    def test_init_service(self):
+        with self.app.app_context():
+            service = init_google_service(FUSION_TABLE_SCOPE)
+
+    def test_replace_rows(self):
+        with self.app.app_context():
+            csvfile = StringIO()
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow([1, 2, 3, 4, 5, 6, 7, 8, 9])
+            replace_rows('1y6rdRvEPXW4r2zXHterHoDrGUVYxnUH_saPMdaCo', csvfile)
+
+
+
+
