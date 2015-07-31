d62f705c39a270bf8b517bf4d81c872004061b68
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-30 21:51:02 -0500

    fixed cors

--- gfsad/__init__.py
+++ gfsad/__init__.py
@@ -50,8 +50,8 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = request.headers.get(
         'Access-Control-Request-Headers', '')
-    response.headers['Access-Control-Allow-Method'] = request.headers.get(
-        'Access-Control-Request-Methods', '')
+    response.headers['Access-Control-Allow-Methods'] = request.headers.get(
+        'Access-Control-Request-Method', '')
 
     # Do nothing for post, patch, delete etc..
     try:
