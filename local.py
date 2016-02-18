b3afdbb5f36c19dc9fd435f45e01e87991d2af35
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-02-17 22:44:18 -0700

    port change and sql echo

--- local.py
+++ local.py
@@ -2,4 +2,5 @@ from gfsad import create_app
 
 if __name__ == "__main__":
     app = create_app('Development')
-    app.run(debug=True, threaded=True)
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.run(debug=True, port=8000, threaded=True)
