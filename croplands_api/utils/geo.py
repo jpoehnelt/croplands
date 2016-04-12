263af4401aa33886e068a99cd42c74fc702ae398
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-04-11 22:07:01 -0700

    add random sampling

--- croplands_api/utils/geo.py
+++ croplands_api/utils/geo.py
@@ -1,5 +1,7 @@
 from geopy.distance import vincenty, Point, VincentyDistance
 import math
+import numpy as np
+from shapely.ops import triangulate
 
 
 def distance(lat1, lon1, lat2, lon2):
@@ -148,3 +150,52 @@ def tile_number_to_degree(x, y, zoom):
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
     lat_deg = math.degrees(lat_rad)
     return lat_deg, lon_deg
+
+
+def uniform_sample(poly, n=100):
+    """
+    Uniformly sample the Delaunay triangulation of a polygon. If the polygon
+    is convex, this will uniformly sample its area.
+    Parameters
+    ----------
+    poly: Shapely Polygon
+    n: Number of points
+    Returns
+    -------
+    [n x 2] numpy array of x-y coordinates that are uniformly distributed
+    over the Delaunay triangulation.
+    """
+    polys = triangulate(poly)
+    # Normalize the areas
+    areas = np.array([p.area for p in polys])
+    areas /= areas.sum()
+
+    # Randomly select a triangle weighted by area
+    # t_inds is the index of the chosen triangle
+    t_inds = np.searchsorted(np.cumsum(areas), np.random.random(n))
+
+    # Randomly sample the area of each triangle according to
+    # P = (1-sqrt(r1))A + (sqrt(r1)(1-r2))B + (sqrt(r1)r2)C
+    # where r1, r2 are sampled uniform [0, 1] and A, B, C are the triangle
+    # vertices
+    # http://math.stackexchange.com/questions/18686/uniform-random-point-in-triangle
+
+    # Compute the coefficients
+    sr1 = np.sqrt(np.random.random(n))  # sr1 is sqrt(r1) above
+    r2 = np.random.random(n)
+    c0 = 1 - sr1
+    c1 = sr1 * (1 - r2)
+    c2 = sr1 * r2
+
+    # Grab the triangle vertices.
+    # v is a 3-element list where each element is [len(polys) x 2]
+    # array of each triangle vertex. It represents, A, B, C above.
+    v = [np.array([p.exterior.coords[i] for p in polys])
+         for i in range(3)]
+
+    # Compute the points. v[i] is [N x 2] and the coefficients are [N x 1]
+    P = (c0[:, np.newaxis] * v[0][t_inds,:] +
+         c1[:, np.newaxis] * v[1][t_inds,:] +
+         c2[:, np.newaxis] * v[2][t_inds,:])
+
+    return P
