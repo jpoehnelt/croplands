af56057edfffb073b19bc2498886a0c5d9896ae7
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-08-10 16:35:00 -0700

    remove message

--- croplands_api/models/base.py
+++ croplands_api/models/base.py
@@ -8,15 +8,6 @@ class BaseModel(db.Model):
     def get_topic(self):
         return str(uuid.uuid4()).replace("-", "")
 
-    def message(self, subject, body):
-        from croplands_api.models.subscription import Message
-        m = Message(subject=subject, body=body, topic=self.topic)
-        db.session.add(m)
-        db.session.commit()
-
-        # auto publish message
-        m.publish()
-
     def save(self):
         db.session.add(self)
         db.session.commit()
